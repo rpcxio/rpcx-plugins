d4ee3caa01033de1acda5ee5948c35453f23c958
chengy <chengy@2wb.com>
2024-06-03 15:35:55 +0800

    #2 追踪增加请求参数

--- go.mod
+++ go.mod
@@ -1,13 +1,16 @@
 module github.com/rpcxio/rpcx-plugins
 
-go 1.18
+go 1.21
+
+toolchain go1.21.7
 
 require (
 	github.com/influxdata/influxdb1-client v0.0.0-20220302092344-a9ab5670611c
 	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475
 	github.com/smallnest/rpcx v1.7.5
-	go.opentelemetry.io/otel v1.8.0
-	go.opentelemetry.io/otel/trace v1.8.0
+	go.opentelemetry.io/otel v1.27.0
+	go.opentelemetry.io/otel/sdk v1.27.0
+	go.opentelemetry.io/otel/trace v1.27.0
 )
 
 require (
@@ -17,7 +20,7 @@ require (
 	github.com/cheekybits/genny v1.0.0 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
-	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
@@ -49,10 +52,11 @@ require (
 	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/xtaci/kcp-go v5.4.20+incompatible // indirect
+	go.opentelemetry.io/otel/metric v1.27.0 // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
 	golang.org/x/net v0.0.0-20220708220712-1185a9018129 // indirect
-	golang.org/x/sys v0.0.0-20220708085239-5a0f0661e09d // indirect
+	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/tools v0.1.11 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
--- server/otel/opentelemetry.go
+++ server/otel/opentelemetry.go
@@ -2,16 +2,23 @@ package otel
 
 import (
 	"context"
+	"fmt"
 	"net"
+	"os"
+	"strconv"
+	"strings"
 
-	"github.com/rpcxio/rpcx-plugins/share"
 	"github.com/smallnest/rpcx/protocol"
 	"github.com/smallnest/rpcx/server"
 	rc "github.com/smallnest/rpcx/share"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/propagation"
+	semconv "go.opentelemetry.io/otel/semconv/v1.18.0"
 	"go.opentelemetry.io/otel/trace"
+
+	"github.com/rpcxio/rpcx-plugins/share"
 )
 
 var (
@@ -25,6 +32,19 @@ type OpenTelemetryPlugin struct {
 	tracer      trace.Tracer
 	propagators propagation.TextMapPropagator
 }
+
+const (
+	instrumentName                            = "github.com/smallnest/rpcx/server"
+	tracingCommonKeyIpIntranet                = `ip.intranet`
+	tracingCommonKeyIpHostname                = `hostname`
+	tracingEventRpcxPreHandleRequest          = "rpcx.pre.handle.request"
+	tracingEventRpcxPreHandleRequestPath      = "rpcx.pre.handle.request.path"
+	tracingEventRpcxPreHandleRequestMetadata  = "rpcx.pre.handle.request.metadata"
+	tracingEventRpcxPreHandleRequestPayload   = "rpcx.pre.handle.request.payload"
+	tracingEventRpcxPostWriteResponse         = "rpcx.post.write.response"
+	tracingEventRpcxPostWriteResponseMetadata = "rpcx.post.write.response.metadata"
+	tracingEventRpcxPostWriteResponsePayload  = "rpcx.post.write.response.payload"
+)
 
 func NewOpenTelemetryPlugin(tracer trace.Tracer, propagators propagation.TextMapPropagator) *OpenTelemetryPlugin {
 	if propagators == nil {
@@ -77,13 +97,29 @@ func (p OpenTelemetryPlugin) PreHandleRequest(ctx context.Context, r *protocol.M
 	spanCtx := share.Extract(ctx, p.propagators)
 	ctx0 := trace.ContextWithSpanContext(ctx, spanCtx)
 
-	ctx1, span := p.tracer.Start(ctx0, "rpcx.service."+r.ServicePath+"."+r.ServiceMethod)
+	ctx1, span := otel.GetTracerProvider().Tracer(
+		instrumentName,
+	).Start(
+		ctx0,
+		"rpcx.service."+r.ServicePath+"."+r.ServiceMethod,
+		trace.WithSpanKind(trace.SpanKindServer),
+	)
+	hostname, _ := os.Hostname()
+	intranetIps, _ := GetIntranetIpArray()
+	intranetIpStr := strings.Join(intranetIps, ",")
+	span.SetAttributes(
+		attribute.String(tracingCommonKeyIpHostname, hostname),
+		attribute.String(tracingCommonKeyIpIntranet, intranetIpStr),
+		semconv.HostName(hostname))
 	share.Inject(ctx1, p.propagators)
 
 	ctx.(*rc.Context).SetValue(share.OpenTelemetryKey, span)
 
-	span.AddEvent("PreHandleRequest")
-
+	span.AddEvent(tracingEventRpcxPreHandleRequest, trace.WithAttributes(
+		attribute.String(tracingEventRpcxPreHandleRequestPath, "rpcx.service."+r.ServicePath+"."+r.ServiceMethod),
+		attribute.String(tracingEventRpcxPreHandleRequestMetadata, fmt.Sprintf("%+v", r.Metadata)),
+		attribute.String(tracingEventRpcxPreHandleRequestPayload, string(r.Payload)),
+	))
 	return nil
 }
 
@@ -91,7 +127,102 @@ func (p OpenTelemetryPlugin) PostWriteResponse(ctx context.Context, req *protoco
 	span := ctx.Value(share.OpenTelemetryKey).(trace.Span)
 	defer span.End()
 
-	span.AddEvent("PostWriteResponse")
+	span.AddEvent(tracingEventRpcxPostWriteResponse, trace.WithAttributes(
+		attribute.String(tracingEventRpcxPostWriteResponseMetadata, fmt.Sprintf("%+v", res.Metadata)),
+		attribute.String(tracingEventRpcxPostWriteResponsePayload, string(res.Payload)),
+	))
+	if err != nil {
+		span.SetStatus(codes.Error, err.Error())
+	} else {
+		span.SetStatus(codes.Ok, "success")
+	}
 
 	return nil
+}
+
+func GetIntranetIpArray() (ips []string, err error) {
+	var (
+		addresses  []net.Addr
+		interFaces []net.Interface
+	)
+	interFaces, err = net.Interfaces()
+	if err != nil {
+		return ips, err
+	}
+	for _, interFace := range interFaces {
+		if interFace.Flags&net.FlagUp == 0 {
+			// interface down
+			continue
+		}
+		if interFace.Flags&net.FlagLoopback != 0 {
+			// loop back interface
+			continue
+		}
+		// ignore warden bridge
+		if strings.HasPrefix(interFace.Name, "w-") {
+			continue
+		}
+		addresses, err = interFace.Addrs()
+		if err != nil {
+			return ips, err
+		}
+		for _, addr := range addresses {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ip = ip.To4()
+			if ip == nil {
+				// not an ipv4 address
+				continue
+			}
+			ipStr := ip.String()
+			if IsIntranet(ipStr) {
+				ips = append(ips, ipStr)
+			}
+		}
+	}
+	return ips, nil
+}
+
+// IsIntranet checks and returns whether given ip an intranet ip.
+//
+// Local: 127.0.0.1
+// A: 10.0.0.0--10.255.255.255
+// B: 172.16.0.0--172.31.255.255
+// C: 192.168.0.0--192.168.255.255
+func IsIntranet(ip string) bool {
+	if ip == "127.0.0.1" {
+		return true
+	}
+	array := strings.Split(ip, ".")
+	if len(array) != 4 {
+		return false
+	}
+	// A
+	if array[0] == "10" || (array[0] == "192" && array[1] == "168") {
+		return true
+	}
+	// C
+	if array[0] == "192" && array[1] == "168" {
+		return true
+	}
+	// B
+	if array[0] == "172" {
+		second, err := strconv.ParseInt(array[1], 10, 64)
+		if err != nil {
+			return false
+		}
+		if second >= 16 && second <= 31 {
+			return true
+		}
+	}
+	return false
 }
