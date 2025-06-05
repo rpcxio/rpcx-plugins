37d5bd9642bba97e04e7081267b67a183be95f63
Syd Xu <prof.syd.xu@gmail.com>
2025-06-05 11:56:38 +0800

    fix(share): metadataSupplier map race

--- share/trace.go
+++ share/trace.go
@@ -2,6 +2,7 @@ package share
 
 import (
 	"context"
+	"sync"
 
 	"github.com/smallnest/rpcx/share"
 	"go.opentelemetry.io/otel/propagation"
@@ -17,19 +18,30 @@ const (
 
 type metadataSupplier struct {
 	metadata map[string]string
+	lock     *sync.RWMutex
 }
 
-var _ propagation.TextMapCarrier = &metadataSupplier{}
+func newMetadataSupplier(mp map[string]string) *metadataSupplier {
+	return &metadataSupplier{metadata: mp, lock: new(sync.RWMutex)}
+}
+
+var _ propagation.TextMapCarrier = newMetadataSupplier(make(map[string]string))
 
 func (s *metadataSupplier) Get(key string) string {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
 	return s.metadata[key]
 }
 
 func (s *metadataSupplier) Set(key string, value string) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
 	s.metadata[key] = value
 }
 
 func (s *metadataSupplier) Keys() []string {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
 	out := make([]string, 0, len(s.metadata))
 	for key := range s.metadata {
 		out = append(out, key)
@@ -46,9 +58,7 @@ func Inject(ctx context.Context, propagators propagation.TextMapPropagator) {
 		}
 	}
 
-	propagators.Inject(ctx, &metadataSupplier{
-		metadata: meta.(map[string]string),
-	})
+	propagators.Inject(ctx, newMetadataSupplier(meta.(map[string]string)))
 }
 
 func Extract(ctx context.Context, propagators propagation.TextMapPropagator) trace.SpanContext {
@@ -60,9 +70,7 @@ func Extract(ctx context.Context, propagators propagation.TextMapPropagator) tra
 		}
 	}
 
-	ctx = propagators.Extract(ctx, &metadataSupplier{
-		metadata: meta.(map[string]string),
-	})
+	ctx = propagators.Extract(ctx, newMetadataSupplier(meta.(map[string]string)))
 
 	return trace.SpanContextFromContext(ctx)
 }
